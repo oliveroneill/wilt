f0784310a678fe81205df2e20dc63f1dc0d4c1a6
Oliver <oliveroneill04@gmail.com>
2018-09-26 18:56:34 +1000

    Change to consistent double quotes

--- constants.js
+++ constants.js
@@ -1,4 +1,4 @@
 // User to query on
 var user = "<ENTER-USERNAME-HERE>";
 // Endpoint to make requests to
-var apiGatewayEndpoint = '<ENTER-API-GATEWAY-URL-HERE>';
+var apiGatewayEndpoint = "<ENTER-API-GATEWAY-URL-HERE>";
