1bd16d5d8294fed42d2dc3e09908a6510dbfbe2a
Oliver <oliveroneill04@gmail.com>
2018-12-22 14:42:23 +1100

    Add script to upload failures from travis

---
+++ upload_failures.py
@@ -0,0 +1,32 @@
+"""
+Upload cypress diffs for inspection.
+
+Useful when Travis fails but local doesn't
+"""
+import os
+import shutil
+import time
+
+import tinys3
+
+
+if __name__ == '__main__':
+    filename = 'wilt-failures_{}'.format(time.time())
+    failureDir = 'cypress/match-screenshots/'
+    if os.path.isdir(failureDir):
+        shutil.make_archive(filename, 'zip', failureDir)
+        filename += '.zip'
+
+        conn = tinys3.Connection(
+            os.environ['AWS_ACCESS_KEY_ID'],
+            os.environ['AWS_SECRET_ACCESS_KEY'],
+            tls=True
+        )
+        with open(filename, 'rb') as f:
+            conn.upload(
+                filename,
+                f,
+                os.environ['UPLOAD_IOS_SNAPSHOT_BUCKET_NAME']
+            )
+    else:
+        print("No failures found")
