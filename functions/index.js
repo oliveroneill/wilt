cc9b39c4fd91cff544557efd2c798ea55d154e6c
Oliver <oliveroneill04@gmail.com>
2019-08-11 21:09:07 +1000

    Improve readability of SQL query

--- functions/index.js
+++ functions/index.js
@@ -232,56 +232,111 @@ exports.getTopArtistPerWeek = functions
   var interval = 'WEEK';
   const bigQuery = new BigQuery();
   const sqlQuery = `
-  WITH subquery AS (
+WITH plays_per_week AS (
+  SELECT
+    period,
+    year,
+    playdate,
+    primary_artist,
+    SUM(count) AS events
+  FROM (
     SELECT
-      period,
-      year,
-      playdate,
-      primary_artist,
-      SUM(count) AS events
+      grouped.period,
+      grouped.year,
+      grouped.playdate,
+      grouped.primary_artist,
+      count
     FROM (
-      SELECT
-        grouped.period,
-        grouped.year,
-        grouped.playdate,
-        grouped.primary_artist,
-        count
-      FROM (
-        wilt_play_history.play_history
-        CROSS JOIN (
-          SELECT 1 AS count
-        )
+      wilt_play_history.play_history CROSS JOIN (
+        SELECT 1 AS count
       )
-      RIGHT JOIN (
+    )
+    RIGHT JOIN (
+      SELECT
+        period_data.period,
+        period_data.year,
+        period_data.playdate,
+        primary_artist
+      FROM (
+        SELECT DISTINCT primary_artist
+        FROM
+          wilt_play_history.play_history
+        WHERE
+          user_id = @user
+          AND UNIX_SECONDS(date) BETWEEN @start AND @end
+      ) as history CROSS JOIN (
         SELECT
-          period_data.period,
-          period_data.year,
-          period_data.playdate,
-          primary_artist
-        FROM (
-          SELECT
-          DISTINCT primary_artist
-          FROM wilt_play_history.play_history
-          WHERE user_id=@user AND UNIX_SECONDS(date) BETWEEN @start AND @end
-        ) as history
-        CROSS JOIN (
-          SELECT period AS playdate, EXTRACT(${extract} FROM period) AS period, EXTRACT(YEAR FROM period) AS year
-          FROM UNNEST(
-              GENERATE_DATE_ARRAY(DATE(TIMESTAMP_SECONDS(@start)), DATE(TIMESTAMP_SECONDS(@end)), INTERVAL 1 ${interval})
+          period AS playdate,
+          EXTRACT(
+            ${extract}
+            FROM period
+          ) AS period,
+          EXTRACT(
+            YEAR
+            FROM
+              period
+          ) AS year
+        FROM
+          UNNEST(
+            GENERATE_DATE_ARRAY(
+              DATE(
+                TIMESTAMP_SECONDS(@start)
+              ),
+              DATE(
+                TIMESTAMP_SECONDS(@end)
+              ),
+              INTERVAL 1 ${interval}
+            )
           ) AS period
-        ) AS period_data
-      ) AS grouped ON EXTRACT(${extract} FROM play_history.date) = grouped.period AND
-        EXTRACT(YEAR FROM play_history.date) = grouped.year AND
-        grouped.primary_artist = play_history.primary_artist AND
-        play_history.user_id = @user
-        WHERE count IS NOT NULL
-    ) GROUP BY period, year, playdate, primary_artist)
-    SELECT MAX(sq.primary_artist) AS top_artist, MAX(sq.events) AS count,
-      FORMAT_DATE("%F", sq.playdate) AS date, FORMAT('%d-%d', sq.period, sq.year) AS week FROM subquery sq,
-    (SELECT MAX(events) AS count, playdate, period, year FROM subquery GROUP BY playdate, period, year) max_results
-    WHERE sq.playdate = max_results.playdate AND sq.events = max_results.count AND
-    sq.period = max_results.period AND sq.year = max_results.year
-    GROUP BY sq.playdate, sq.period, sq.year ORDER BY sq.playdate DESC;`;
+      ) AS period_data
+    ) AS grouped ON EXTRACT(
+      ${extract}
+      FROM play_history.date
+    ) = grouped.period
+    AND EXTRACT(
+      YEAR
+      FROM play_history.date
+    ) = grouped.year
+    AND grouped.primary_artist = play_history.primary_artist
+    AND play_history.user_id = @user
+    WHERE count IS NOT NULL
+  )
+  GROUP BY
+    period,
+    year,
+    playdate,
+    primary_artist
+)
+
+SELECT
+  MAX(per_week.primary_artist) AS top_artist,
+  MAX(per_week.events) AS count,
+  FORMAT_DATE("%F", per_week.playdate) AS date,
+  FORMAT('%d-%d', per_week.period, per_week.year) AS week
+FROM
+  plays_per_week per_week,
+  (
+    SELECT
+      MAX(events) AS count,
+      playdate,
+      period,
+      year
+    FROM plays_per_week
+    GROUP BY
+      playdate,
+      period,
+      year
+  ) max_results
+WHERE
+  per_week.playdate = max_results.playdate
+  AND per_week.events = max_results.count
+  AND per_week.period = max_results.period
+  AND per_week.year = max_results.year
+GROUP BY
+  per_week.playdate,
+  per_week.period,
+  per_week.year
+ORDER BY per_week.playdate DESC;`;
 
   var job;
   return bigQuery.createQueryJob({
