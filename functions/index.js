21cc2f320537884625773eb629b6780fc0857722
Oliver <oliveroneill04@gmail.com>
2019-08-11 13:42:14 +1000

    Format SQL query better
    I'm really not sure how to make SQL readable.
    I thought about splitting this up into multiple queries but
    I'm not sure it would simplify it since most of this is just
    grouping based on date

--- functions/index.js
+++ functions/index.js
@@ -101,49 +101,82 @@ exports.playsPerArtist = functions
   }
   const bigQuery = new BigQuery();
   const sqlQuery = `
-  SELECT primary_artist, ARRAY_AGG(playdate) AS dates, ARRAY_AGG(events) AS events FROM (
-    SELECT
-      period,
-      FORMAT_DATE("%F", playdate) AS playdate,
-      primary_artist,
-      SUM(count) AS events
-    FROM (
+  SELECT
+    primary_artist,
+    ARRAY_AGG(playdate) AS dates,
+    ARRAY_AGG(events) AS events
+  FROM (
       SELECT
-        grouped.period,
-        grouped.year,
-        grouped.playdate,
-        grouped.primary_artist,
-        IFNULL(count, 0) AS count
+        period,
+        FORMAT_DATE("%F", playdate) AS playdate,
+        primary_artist,
+        SUM(count) AS events
       FROM (
-        wilt_play_history.play_history
-        CROSS JOIN (
-          SELECT 1 AS count
-        )
-      )
-      RIGHT JOIN (
-        SELECT
-          period_data.period,
-          period_data.year,
-          period_data.playdate,
-          primary_artist
-        FROM (
           SELECT
-          DISTINCT primary_artist
-          FROM wilt_play_history.play_history
-          WHERE user_id=@user AND UNIX_SECONDS(date) BETWEEN @start AND @end
-        ) as history
-        CROSS JOIN (
-          SELECT period AS playdate, EXTRACT(${extract} FROM period) AS period, EXTRACT(YEAR FROM period) AS year
-          FROM UNNEST(
-              GENERATE_DATE_ARRAY(DATE(TIMESTAMP_SECONDS(@start)), DATE(TIMESTAMP_SECONDS(@end)), INTERVAL 1 ${interval})
-          ) AS period
-        ) AS period_data
-      ) AS grouped ON EXTRACT(${extract} FROM play_history.date) = grouped.period AND
-        EXTRACT(YEAR FROM play_history.date) = grouped.year AND
-        grouped.primary_artist = play_history.primary_artist AND
-        play_history.user_id = @user
-    ) GROUP BY period, year, playdate, primary_artist ORDER BY period, year
-  ) GROUP BY primary_artist`;
+            grouped.period,
+            grouped.year,
+            grouped.playdate,
+            grouped.primary_artist,
+            IFNULL(count, 0) AS count
+          FROM (
+            wilt_play_history.play_history
+            CROSS JOIN (
+              SELECT 1 AS count
+            )
+          )
+          RIGHT JOIN (
+            SELECT
+              period_data.period,
+              period_data.year,
+              period_data.playdate,
+              primary_artist
+            FROM (
+                SELECT
+                  DISTINCT primary_artist
+                FROM
+                  wilt_play_history.play_history
+                WHERE
+                  user_id = @user
+                  AND UNIX_SECONDS(date) BETWEEN @start AND @end
+            ) as history CROSS JOIN (
+              SELECT
+                period AS playdate,
+                EXTRACT(${extract} FROM period) AS period,
+                EXTRACT(YEAR FROM period) AS year
+              FROM
+                UNNEST(
+                  GENERATE_DATE_ARRAY(
+                    DATE(
+                      TIMESTAMP_SECONDS(@start)
+                    ),
+                    DATE(
+                      TIMESTAMP_SECONDS(@end)
+                    ),
+                    INTERVAL 1 ${interval}
+                  )
+                ) AS period
+            ) AS period_data
+          ) AS grouped ON EXTRACT(
+            ${extract}
+            FROM play_history.date
+          ) = grouped.period
+          AND EXTRACT(
+            YEAR
+            FROM play_history.date
+          ) = grouped.year
+          AND grouped.primary_artist = play_history.primary_artist
+          AND play_history.user_id = @user
+      )
+      GROUP BY
+        period,
+        year,
+        playdate,
+        primary_artist
+      ORDER BY
+        period,
+        year
+  )
+  GROUP BY primary_artist`;
 
   var job;
   return bigQuery.createQueryJob({
